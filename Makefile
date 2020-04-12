4f4aba778b8584705a1cf7a5009ef4782f3a1892
Xavi Del Campo <xavi.dcr@tutanota.com>
2020-04-13 01:46:20 +0200

    First working example with std.fmt.bufPrint

--- Makefile
+++ Makefile
@@ -1,3 +1,8 @@
 all:
 	zig build-exe main.zig -target mipsel-freestanding-gnueabi -mcpu mips1+soft_float --linker-script playstation.x
 	~/psxsdk/tools/elf2exe main zig.exe -mark_eur
+	mkdir -p cdimg/
+	cp zig.exe cdimg/
+	mkisofs -o zig.iso -V zig -sysid PLAYSTATION cdimg/
+	mkpsxiso zig.iso zig.bin /usr/local/psxsdk/share/licenses/infoeur.dat -s
+	pcsxr -nogui -psxout -cdfile zig.cue
--- main.zig
+++ main.zig
@@ -1,17 +1,20 @@
-export fn __start() void {
+// I do not know why, but even if _start is the entry point it
+// must be placed into a separate section...
+extern var __bss_end: u8;
+extern var __bss_start: u8;
+
+export fn _start() linksection(".main") noreturn {
     {
         const sz = @ptrToInt(&__bss_end) - @ptrToInt(&__bss_start);
         @memset(@ptrCast([*]u8, &__bss_start), 0, sz);
     }
+
     main();
 }
 
-extern var __bss_end: u8;
-extern var __bss_start: u8;
-
 pub fn puts(str: [*:0]const u8) c_int {
     return asm volatile (
-        \\ li $9, 0x3e
+        \\ li $9, 0x3f
         \\ j 0xa0
         \\ nop
         : [ret] "={r2}" (-> c_int)
@@ -19,13 +22,26 @@ pub fn puts(str: [*:0]const u8) c_int {
     );
 }
 
+fn fmtNum(num: u32, buffer: []u8) ![*:0]const u8 {
+    const std = @import("std");
+    const formatted = try std.fmt.bufPrint(buffer, "{}\n\x00", .{num});
+    return @ptrCast([*:0]const u8, formatted.ptr);
+}
+
 fn main() noreturn {
-    while (true) {
-        _ = puts("yo");
-    }
+    const a = 486;
+    var buffer: [16]u8 = undefined;
+    _ = puts("yo\n");
+    _ = puts(fmtNum(a, &buffer) catch "error\n");
+    _ = puts("num\n");
+    _ = puts("num2\n");
+    const b = puts(fmtNum(98, &buffer) catch "error\n");
+    _ = puts(fmtNum(@intCast(u32, b), &buffer) catch "error\n");
+
+    while (true) {}
 }
 
 //const builtin = @import("builtin");
 //pub fn panic(msg: []const u8, error_return_trace: ?*builtin.StackTrace) noreturn {
-    //while (true) {}
+//    while (true) {}
 //}
